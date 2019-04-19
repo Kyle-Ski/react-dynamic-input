24fdf0161f9387df490b960f2c1aee598e02b771
Kyle Czajkowski <skiroyjenkins@gmail.com>
2019-04-19 09:45:11 -0600

    initial commit

---
+++ src/DynamicInput.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react"
+
+class DynamicInput extends Component {
+  render() {
+    return <div className="DynamicInput">Hello</div>
+  }
+}
+
+export default DynamicInput
