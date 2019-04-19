928d0fd624971b996afa0b5e3be768f886e63518
Kyle Czajkowski <skiroyjenkins@gmail.com>
2019-04-19 12:10:03 -0600

    completed functionality of DynamicInput with props pasing propperly, WIP: styling

--- src/DynamicInput.js
+++ src/DynamicInput.js
@@ -1,9 +1,98 @@
-import React, { Component } from "react"
+import React, { useState } from "react"
 
-class DynamicInput extends Component {
-  render() {
-    return <div className="DynamicInput">Hello</div>
+const DynamicInput = ({
+  addButtonText,
+  setInput,
+  input,
+  buttonText,
+  onSubmit,
+  inputName,
+  addPosition
+}) => {
+  const [show, setShow] = useState(false)
+  const addInput = e => {
+    e.preventDefault()
+    setInput([...input, { name: "" }])
+  }
+  const removeInput = index => e => {
+    e.preventDefault()
+    const items = input.filter((item, subIndex) => index !== subIndex)
+    setInput(items)
+    console.log(index, items)
+  }
+
+  const inputChange = index => e => {
+    const elements = input.map((element, subIndex) => {
+      if (index !== subIndex) {
+        return element
+      } else {
+        return { ...element, [inputName]: e.target.value }
+      }
+    })
+    setInput(elements)
+  }
+  if (addPosition === "bottom") {
+    return (
+      <div className="DynamicInput">
+        {input.map((item, index) => (
+          <div key={(index + 1) * 2}>
+            <input
+              type="text"
+              placeholder="text here..."
+              onChange={inputChange(index)}
+              value={item[inputName]}
+            />
+            <button onClick={removeInput(index)}>Remove</button>
+          </div>
+        ))}
+        <button onClick={onSubmit}>{buttonText}</button>
+        <button onClick={addInput}>{addButtonText}</button>
+      </div>
+    )
+  } else {
+    return (
+      <div className="DynamicInput">
+        <button onClick={addInput}>{addButtonText}</button>
+        {input.map((item, index) => (
+          <div key={(index + 1) * 2}>
+            <input
+              type="text"
+              placeholder="text here..."
+              onChange={inputChange(index)}
+              value={item[inputName]}
+            />
+            <button onClick={removeInput(index)}>Remove</button>
+          </div>
+        ))}
+        <button onClick={onSubmit}>{buttonText}</button>
+      </div>
+    )
   }
 }
 
-export default DynamicInput
+const App = () => {
+  const [input, setInput] = useState([{ name: "" }])
+
+  const showState = () => {
+    alert(input.map(item => item.name))
+  }
+  return (
+    <div>
+      <DynamicInput
+        inputName="name"
+        addButtonText="Add Input"
+        setInput={setInput}
+        input={input}
+        onSubmit={showState}
+        submitColor="green"
+        buttonText="Submit"
+        addPosition="bottom"
+      />
+      {input.map(item => (
+        <p>{item.name}</p>
+      ))}
+    </div>
+  )
+}
+
+export default App
